b3c7d04aa17922ec3ec06224a580c29dee0bf780
Holt Crews <holtcrews47@gmail.com>
2024-01-03 09:18:57 -0500

    feat(init): Initial bubbletea setup
    Created a main.go file and the required functions for bubbletea to run

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func initialModel() model {
+	return model{}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Hello, World"
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
