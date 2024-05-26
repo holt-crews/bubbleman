19eec2650fd2998b521ff044a85750bee0317dea
Holt Crews <holtcrews47@gmail.com>
2024-05-26 16:04:51 -0500

    Update dependencies and rename files

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/holt-crews/bubbleman
 
-go 1.21.5
+go 1.22
 
 require (
-	github.com/charmbracelet/bubbles v0.17.1
-	github.com/charmbracelet/bubbletea v0.25.0
-	github.com/charmbracelet/lipgloss v0.9.1
+	github.com/charmbracelet/bubbles v0.18.0
+	github.com/charmbracelet/bubbletea v0.26.3
+	github.com/charmbracelet/lipgloss v0.11.0
 	github.com/erikgeiser/promptkit v0.9.0
 	github.com/mattn/go-runewidth v0.0.15
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6
@@ -16,14 +16,19 @@ require (
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/charmbracelet/x/ansi v0.1.1 // indirect
+	github.com/charmbracelet/x/input v0.1.1 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.2 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/term v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
--- tui/tui.go
+++ tui/tui.go
@@ -2,32 +2,216 @@ package tui
 
 import (
 	"fmt"
+	"io"
 	"log"
-	"os"
+	"net/http"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/erikgeiser/promptkit/selection"
+
+	"github.com/holt-crews/bubbleman/helpers"
 )
 
-// StartTea the entry point for the UI. Initializes the model.
-func StartTea() error {
-	if f, err := tea.LogToFile("debug.log", "help"); err != nil {
-		fmt.Println("Couldn't open a file for logging:", err)
-		os.Exit(1)
-	} else {
-		defer func() {
-			err = f.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+// consider just wrapping everything into a bubble tea Model in a "components package"
+type Component interface {
+	// Init() tea.Cmd
+	// for some unknown reason, bubbles don't actually implement the tea.Model interface, missing Init()
+	// Update(tea.Msg) (tea.Model, tea.Cmd)
+	View() string
+}
+
+func newUrlbar() textinput.Model {
+	t := textinput.New()
+	t.Prompt = ""
+	t.Placeholder = "https://api.something.com/v1/users"
+	t.Cursor.Style = cursorStyle
+	t.KeyMap.DeleteWordBackward.SetEnabled(false)
+	t.Blur()
+	return t
+}
+
+func (m *model) newResponseView() viewport.Model {
+	m.response = viewport.New(1, 1)
+	// currently faded text but will want color when a response is received
+	m.response.Style = focusedBorderStyle.Foreground(gray)
+	m.response.SetContent(fmt.Sprintf("{\"response\": \"OK\"...}"))
+	m.response.MouseWheelEnabled = true
+	return m.response
+}
+
+func newTextarea() textarea.Model {
+	t := textarea.New()
+	t.Prompt = ""
+	t.Placeholder = "Type something"
+	t.ShowLineNumbers = true
+	t.Cursor.Style = cursorStyle
+	t.FocusedStyle.Placeholder = focusedPlaceholderStyle
+	t.BlurredStyle.Placeholder = placeholderStyle
+	t.FocusedStyle.CursorLine = cursorLineStyle
+	t.FocusedStyle.Base = focusedBorderStyle
+	t.BlurredStyle.Base = focusedBorderStyle
+	t.FocusedStyle.EndOfBuffer = endOfBufferStyle
+	t.BlurredStyle.EndOfBuffer = endOfBufferStyle
+	t.KeyMap.DeleteWordBackward.SetEnabled(false)
+	t.KeyMap.LineNext = key.NewBinding(key.WithKeys("down"))
+	t.KeyMap.LinePrevious = key.NewBinding(key.WithKeys("up"))
+	t.Blur()
+	return t
+}
+
+type model struct {
+	keymap       keymap
+	help         help.Model
+	requestBody  textarea.Model
+	httpMethod   string
+	urlbar       textinput.Model
+	response     viewport.Model
+	viewReady    bool
+	selection    *selection.Model[string]
+	methodToggle bool
+	components   []Component
+	// focus       int  // will probably want to come back to this when all components are laid out
+}
+
+func InitialRequest() model {
+	sel := selection.New("Items", httpMethods)
+	sel.Filter = nil
+
+	m := model{
+		urlbar:      newUrlbar(),
+		httpMethod:  "GET",
+		requestBody: newTextarea(),
+		help:        help.New(),
+		keymap:      Keymap,
+		selection:   selection.NewModel(sel),
+		components:  []Component{newUrlbar(), newTextarea()},
+	}
+	m.selection.Init()
+
+	// initially focus on requestBody section
+	m.requestBody.Focus()
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		rCmd    tea.Cmd
+		uCmd    tea.Cmd
+		respCmd tea.Cmd
+	)
+	var cmds []tea.Cmd
+
+	m.requestBody, rCmd = m.requestBody.Update(msg)
+	m.urlbar, uCmd = m.urlbar.Update(msg)
+	m.response, respCmd = m.response.Update(msg)
+	cmds = append(cmds, rCmd, uCmd, respCmd)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keymap.Quit):
+			return m, tea.Quit
+		case key.Matches(msg, m.keymap.Request):
+			m.urlbar.Blur()
+			cmd := m.requestBody.Focus()
+			cmds = append(cmds, cmd)
+		case key.Matches(msg, m.keymap.Url):
+			m.requestBody.Blur()
+			cmd := m.urlbar.Focus()
+			cmds = append(cmds, cmd)
+		case key.Matches(msg, m.keymap.HttpMethod):
+			m.methodToggle = !m.methodToggle
+		case key.Matches(msg, m.keymap.Send):
+			m.urlbar.Blur()
+			m.requestBody.Blur()
+			resp := m.sendRequest()
+			m.response.SetContent(resp)
+			// cmds = append(cmds, cmd)
+		}
+	case tea.WindowSizeMsg:
+		WindowSize = msg
+		if !m.viewReady {
+			m.newResponseView()
+			m.viewReady = true
+		}
+		m.sizeInputs()
 	}
 
-	m := InitialRequest()
-	program := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseAllMotion())
+	// Update all text
+	return m, tea.Batch(cmds...)
+}
 
-	if _, err := program.Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
+// needs to be a pointer receiver in order to update
+func (m *model) sizeInputs() {
+	top, right, bottom, left := DocStyle.GetMargin()
+	remainingHeight := WindowSize.Height - top - bottom
+	remainingWidth := WindowSize.Width - left - right
+
+	m.urlbar.Width = remainingWidth
+
+	m.requestBody.SetWidth(remainingWidth)
+	// gotta be careful with division because it floors it, the divide by 3 messes with it, consider alternate ways
+	m.requestBody.SetHeight(2*(remainingHeight/3) - 2)
+	// there's a bug in viewport: https://github.com/charmbracelet/bubbles/pull/388
+	// TODO: ideally the "- 2" is dynamic based on the height of the http method box
+	m.response.Height = (remainingHeight / 3) - 2
+	// .SetWidth() and .Width are calculated differently. 2 seems to be magic difference for my case
+	m.response.Width = remainingWidth + 2
+}
+
+func (m model) View() string {
+	doc := strings.Builder{}
+	help := m.help.ShortHelpView([]key.Binding{
+		m.keymap.Request,
+		m.keymap.Url,
+		m.keymap.Send,
+		m.keymap.Quit,
+	})
+
+	bar := lipgloss.JoinHorizontal(
+		lipgloss.Center,
+		methodBoxStyle.Render(m.httpMethod),
+		m.urlbar.View(),
+	)
+	lipgloss.Height(bar)
+	requestInputs := lipgloss.JoinVertical(
+		lipgloss.Top,
+		bar,
+		m.requestBody.View(),
+		m.response.View(),
+	)
+	doc.WriteString(requestInputs)
+	if m.methodToggle {
+		overlay := helpers.PlaceOverlay(10, 10, m.selection.View(), doc.String(), false)
+		doc.WriteString(overlay)
 	}
-	return nil
+	doc.WriteString("\n")
+	doc.WriteString(help)
+
+	return DocStyle.Render(doc.String())
+}
+
+func (m model) sendRequest() string {
+	resp, err := http.Get(m.urlbar.Value())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+
+	return bodyString
 }
